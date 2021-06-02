753ef572524f40e6399e8633072fbc5ce6b7ea30
LePhan <le.phan@sunbytes.vn>
2021-06-03 05:01:28 +0700

    Nested container components

---
+++ taskbox/src/components/InboxScreen.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { PureInboxScreen } from './InboxScreen';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  component: PureInboxScreen,
+  title: 'InboxScreen',
+};
+
+const Template = args => <PureInboxScreen {...args} />;
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Something',
+};
