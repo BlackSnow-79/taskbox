2103c6f43ac51280eff351d590416bd48ab349af
LePhan <le.phan@sunbytes.vn>
2021-06-03 12:48:54 +0700

    change task background to red

--- taskbox/src/components/Task.stories.js
+++ taskbox/src/components/Task.stories.js
@@ -2,34 +2,34 @@ import Task from './Task';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  component: Task,
-  title: 'Task',
+    component: Task,
+    title: 'Task',
 };
 
 const Template = args => <Task {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  task: {
-    id: '1',
-    title: 'Test Task',
-    state: 'TASK_INBOX',
-    updatedAt: new Date(2021, 0, 1, 9, 0),
-  },
+    task: {
+        id: '1',
+        title: 'Test Task',
+        state: 'TASK_INBOX',
+        updatedAt: new Date(2021, 0, 1, 9, 0),
+    },
 };
 
 export const Pinned = Template.bind({});
 Pinned.args = {
-  task: {
-    ...Default.args.task,
-    state: 'TASK_PINNED',
-  },
+    task: {
+        ...Default.args.task,
+        state: 'TASK_PINNED',
+    },
 };
 
 export const Archived = Template.bind({});
 Archived.args = {
-  task: {
-    ...Default.args.task,
-    state: 'TASK_ARCHIVED',
-  },
+    task: {
+        ...Default.args.task,
+        state: 'TASK_ARCHIVED',
+    },
 };
